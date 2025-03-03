1e2070a86671469948fd2697ced4ed93023360e5
Shago01 <kgsanty2001@gmail.com>
2025-03-03 12:30:22 -0500

    fix: validar token antes de acceder a rutas protegidas

--- backend/src/routes/v1/nazareno.routes.ts
+++ backend/src/routes/v1/nazareno.routes.ts
@@ -1,8 +1,16 @@
 import nazarenoController from '@controllers/nazareno.controller';
+import { verifyRol } from '@middlewares/verifyRol';
+import verifyToken from '@middlewares/verifyToken';
+import rol from '@utils/enum/rols.enum';
 import { Router } from 'express';
 
 const nazarenoRoutes = Router();
 
-nazarenoRoutes.post('/', nazarenoController.createNazareno);
+nazarenoRoutes.post(
+  '/',
+  verifyToken,
+  verifyRol([rol.REGISTER]),
+  nazarenoController.createNazareno,
+);
 
 export default nazarenoRoutes;
--- backend/src/routes/v1/user.routes.ts
+++ backend/src/routes/v1/user.routes.ts
@@ -1,10 +1,28 @@
 import userController from '@controllers/user.controller';
 import hashPassword from '@middlewares/hashpassword';
+import { verifyRol } from '@middlewares/verifyRol';
+import verifyToken from '@middlewares/verifyToken';
+import rol from '@utils/enum/rols.enum';
 import { Router } from 'express';
 
 const userRoutes = Router();
 
-userRoutes.post('/signup', hashPassword, userController.singUser);
+userRoutes.post(
+  '/newRoot',
+  verifyToken,
+  verifyRol([rol.ROOT]),
+  hashPassword,
+  userController.singUser,
+);
+
+userRoutes.post(
+  '/newUser',
+  verifyToken,
+  verifyRol([rol.ADMIN, rol.ROOT]),
+  hashPassword,
+  userController.singUser,
+);
+
 userRoutes.post('/login', hashPassword, userController.loginUser);
 
 export default userRoutes;
