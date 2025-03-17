2cec1695e23bd1fbfbaa07834860104e3c480995
Shago01 <kgsanty2001@gmail.com>
2025-03-17 14:26:28 -0500

    feat: agregar funcionalidad de cierre de sesión en el sidebar y mejorar la estructura de los elementos del menú

--- frontend/src/components/ui/SidebarItem.jsx
+++ frontend/src/components/ui/SidebarItem.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 
-function SidebarItem({ to, text, style, icon: Icon }) {
+function SidebarItem({ to, text, style, icon: Icon, handleClick }) {
+  console.log(handleClick);
+
   return (
-    <Link to={to} className={style}>
+    <Link to={to} className={style} onClick={handleClick}>
       {Icon && <Icon size={20} />}
       <span>{text}</span>
     </Link>
--- frontend/src/layouts/dashboard/SidebarDashborad.jsx
+++ frontend/src/layouts/dashboard/SidebarDashborad.jsx
@@ -1,77 +1,79 @@
 import { FiHome, FiLayers, FiSettings, FiLogOut } from 'react-icons/fi';
 import SidebarItem from '../../components/ui/SidebarItem';
 import { SidebarDropdown } from '../../components/ui/SidebarDropdown';
+import useAuth from '../../hooks/auth/useAuth';
 
 const styles = {
-  based:
-    'flex items-center gap-3 p-3 rounded-lg cursor-pointer text-gray-700 hover:bg-gradient-to-r',
-  primary: ' from-blue-500 to-blue-700 hover:text-white transition-all',
-  danger: ' from-red-500 to-red-700 hover:text-white transition-all mt-6',
+  base: 'flex items-center gap-3 p-3 rounded-lg cursor-pointer text-gray-700 hover:bg-gradient-to-r transition-all',
+  primary: 'from-blue-500 to-blue-700 hover:text-white',
+  danger: 'from-red-500 to-red-700 hover:text-white mt-6',
 };
-const items = [
-  {
-    icon: FiHome,
-    style: styles.based + styles.primary,
-    text: 'Inicio',
-    children: [],
-  },
-  {
-    icon: FiLayers,
-    style: styles.based + styles.primary,
-    text: 'Categorías',
-    children: [
-      {
-        icon: FiLayers,
-        style: styles.based + styles.primary,
-        text: 'Categorías',
-      },
-      {
-        icon: FiLayers,
-        style: styles.based + styles.primary,
-        text: 'Categorías',
-      },
-      {
-        icon: FiLayers,
-        style: styles.based + styles.primary,
-        text: 'Categorías',
-      },
-    ],
-  },
-  {
-    icon: FiSettings,
-    style: styles.based + styles.primary,
-    text: 'Configuración',
-    children: [],
-  },
-  {
-    icon: FiLogOut,
-    style: styles.based + styles.danger,
-    text: 'Cerrar sesión',
-    children: [],
-  },
-];
 
 export function SidebarDashboard() {
+  const { logoutUser } = useAuth();
+
+  const menuItems = [
+    {
+      icon: FiHome,
+      text: 'Inicio',
+      style: `${styles.base} ${styles.primary}`,
+      to: '/dashboard',
+    },
+    {
+      icon: FiLayers,
+      text: 'Categorías',
+      style: `${styles.base} ${styles.primary}`,
+      children: [
+        {
+          icon: FiLayers,
+          text: 'Categorías A',
+          to: '/categorias/a',
+          style: `${styles.base} ${styles.primary}`,
+        },
+        {
+          icon: FiLayers,
+          text: 'Categorías B',
+          to: '/categorias/b',
+          style: `${styles.base} ${styles.primary}`,
+        },
+        {
+          icon: FiLayers,
+          text: 'Categorías C',
+          to: '/categorias/c',
+          style: `${styles.base} ${styles.primary}`,
+        },
+      ],
+    },
+    {
+      icon: FiSettings,
+      text: 'Configuración',
+      style: `${styles.base} ${styles.primary}`,
+      to: '/configuracion',
+    },
+    {
+      icon: FiLogOut,
+      text: 'Cerrar sesión',
+      style: `${styles.base} ${styles.danger}`,
+      handleClick: logoutUser,
+    },
+  ];
+
   return (
-    <aside className="w-64 bg-white shadow-lg p-5 ">
+    <aside className="w-64 bg-white shadow-lg p-5">
       <h2 className="text-xl font-semibold text-gray-700 mb-6">Menú</h2>
       <nav>
         <ul className="space-y-4">
-          {items.map((item, i) => {
-            if (item.children.length === 0) {
-              return (
-                <li key={i}>
-                  <SidebarItem {...item} />
-                </li>
-              );
-            } else {
-              return (
-                <li key={i}>
-                  <SidebarDropdown {...item} />
-                </li>
-              );
-            }
-          })}
+          {menuItems.map((item, i) =>
+            item.children ? (
+              <li key={i}>
+                <SidebarDropdown {...item} />
+              </li>
+            ) : (
+              <li key={i}>
+                <SidebarItem {...item} />
+              </li>
+            ),
+          )}
         </ul>
       </nav>
     </aside>
